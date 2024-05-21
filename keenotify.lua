500099016d0d0ed86ca80691402804293c0a9deb
Nghia11n <156570662+Nghia11n@users.noreply.github.com>
2024-05-21 16:00:53 +0700

    Add files via upload

---
+++ keenotify.lua
@@ -0,0 +1,22 @@
+spawn(function() 
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    repeat
+        task.wait()
+    until game.Players
+    repeat
+        task.wait()
+    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
+    wait(1.5)
+    require(game.ReplicatedStorage.Notification).new("<Color=Red>Welcome To Kee Hub<Color=/>"):Display()
+    wait(.5)
+    require(game.ReplicatedStorage.Notification).new("<Color=Red>Ra Mắt Chính Thức Ngày<Color=/>"):Display()
+    wait(.14)
+    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>YT: Nghiarobloxtt<Color=/>"):Display()
+    wait(.24)
+    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>Hãy Tham Gia Discord Của Tôi Nhé<Color=/>"):Display()
+    wait(.29)
+    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>https://discord.gg/BxjqHMMsTb<Color=/>"):Display()
+    wait(.36)
+end)
