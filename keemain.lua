aa27a7e44b14d4706ad4ed10e2a3a60a3b862cb2
Nghia11n <156570662+Nghia11n@users.noreply.github.com>
2024-06-16 13:27:54 +0700

    Add files via upload

---
+++ keemain.lua
@@ -0,0 +1,8 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruit
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Nghia11n/Kee-Hub/main/keebloxfruits.lua"))()
+elseif placeId == 142823291 then
+	--Murder Mystery 2
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Nghia11n/Kee-Hub/main/keemurderer2.lua"))()
+end
